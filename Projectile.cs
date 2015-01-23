b7ad418101439c5288d11b0fc172bd582019f3c2
pscorgie <s031789c@student.staffs.ac.uk>
2015-01-23 10:17:27 +0000

    + Projectile & ProjectileManager classes [PS]
    added basic components for each class

---
+++ Projectile.cs
@@ -0,0 +1,62 @@
+using System;
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+namespace TheATeam
+{
+	public class Projectile
+	{
+		public TextureInfo bulletTex;
+		public SpriteUV bulletSprite;
+		public Vector2 position;
+		public Vector2 velocity;
+		public bool collided;
+		
+		public Projectile (Scene scene, Vector2 pos, Vector2 vel)
+		{
+			bulletTex = new TextureInfo("/Application/Assets/fireBullet.png");
+			bulletSprite = new SpriteUV(bulletTex);
+			bulletSprite.Quad.S = turretTex.TextureSizef;
+			bulletSprite.CenterSprite(TRS.Local.Center);
+			
+			scene.AddChild(turret);
+			
+			position = pos;
+			velocity = vel;
+			collided = false;
+		}
+		
+		public void Update(float dt)
+		{
+			position = new Vector2(velocity.X * dt, velocity.Y *dt);
+		}
+		
+		public bool hasCollided(Vector2 objectPosition, Vector2 objectSize)
+		{
+			// Collision for objects that are centred
+			
+			Bounds2 bulletBounds = bulletSprite.Quad.Bounds2();
+			float bulletWidth = bulletBounds.Point11.X;
+			float bulletHeight = bulletBounds.Point11.Y;
+			
+			float objectWidth = objectSize.X;
+			float objectHeight = objectSize.Y;
+			
+			if((position.X + bulletWidth) < objectPosition.X - objectWidth)
+				return false;
+			else if(position.X - bulletWidth > (objectPosition.X + objectWidth))
+				return false;
+			else if((position.Y + bulletHeight) < objectPosition.Y - objectHeight)
+				return false;
+			else if(position.Y - bulletHeight > (objectPosition.Y + objectHeight))
+				return false;
+			else 
+				return true;
+			
+		}
+	}
+}
+
---
+++ ProjectileManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+namespace TheATeam
+{
+	public class ProjectileManager
+	{
+		private static ProjectileManager instance = new ProjectileManager();
+		private static List<Projectile> projectiles;
+		private Scene scene;
+		
+		private ProjectileManager ()
+		{
+		}
+		
+		public static ProjectileManager Instance
+		{
+			get{return instance;}
+		}
+		
+		public void Shoot(Vector2 playerPos, Vector2 direction)
+		{
+			float speed = 5.0f;
+			Vector2 Velocity = new Vector2(direction.X * speed, direction.Y * speed);
+			Projectile newProjectile = new Projectile(scene, playerPos, Velocity);
+			projectiles.Add(newProjectile);
+		}
+		
+		public void Update(float dt)
+		{
+			foreach(Projectile projectile in projectiles)
+			{
+				projectile.Update(dt);
+			}
+			for(int i = 0; i < projectiles.Count(); i++)
+			{
+				if(projectiles[i].collided)
+				{
+					projectiles.RemoveAt(i);
+					i--;
+				}
+			}
+		}
+		public void ProjectileCollision(Vector2 pos, Vector2 size)
+		{
+			foreach(Projectile projectile in projectiles)
+			{
+				if(projectile.hasCollided(pos, size))
+					projectile.collided = true;
+			}
+		}
+	}
+}
+
