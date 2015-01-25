7e607d6b92124abdee647259c736e16cbabdbab0
Luis Checa <c008101c@student.staffs.ac.uk>
2015-01-25 20:59:45 +0000

    [+] Added previous assets and classes [LC]

---
+++ Info.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PairedGame
+{
+	public static class Info
+	{
+		private static Random rnd = new Random();
+		
+		public static double ItemSpawnRate { get; set; }
+		
+		public static float TotalGameTime { get; set; }
+		
+		public static Random Rnd { get { return rnd; } }
+		
+		public static float CameraHeight { get; set; }
+		
+		public static Sce.PlayStation.Core.Vector2 CameraCentre { get; set; }
+		
+		public static bool LevelClear { get; set; }
+
+		public static int LevelNumber { get; set; }
+
+		public static int MaxLevels { get { return 3; } }
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,156 @@
+using System;
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+namespace PairedGame
+{
+	public class Player: EntityAlive
+	{
+		private static int PLAYER_INDEX = 0;
+		
+		public Player(Vector2 position):
+			base(PLAYER_INDEX, position, new Vector2i(0, 1))
+		{
+			IsDefending = false;
+			Stats.Lives = 5;
+		}
+		
+		override public void Update(float dt)
+		{
+	
+			// Handle battle
+			base.Update(dt);
+			
+			// Handle Death
+//			if(!IsAlive)
+//				SceneManager.ReplaceUIScene(new DeadUI());
+			
+			// Handle movement/attacks
+			HandleInput();
+			
+			// Find current tile and apply collision
+			HandleCollision();
+			
+			// Apply the movement
+			Position = Position + MoveSpeed;
+			// Make camera follow the player
+			Info.CameraCentre = Position;
+		}
+		
+		public AttackStatus Attack { get { return attackState; } }
+		
+		private static float MoveDelta = 2f;
+		
+		private void HandleInput()
+		{
+			var gamePadData = GamePad.GetData(0);
+			// Apply direction and animation
+			if((gamePadData.Buttons & GamePadButtons.Left) != 0)
+			{
+				MoveSpeed.X = -MoveDelta;
+				// Set animation range.
+				TileRangeX = new Vector2i(6, 7);
+			}
+			if((gamePadData.Buttons & GamePadButtons.Right) != 0)
+			{
+				MoveSpeed.X = MoveDelta;
+				TileRangeX = new Vector2i(4, 5);
+			}
+			if((gamePadData.Buttons & GamePadButtons.Up) != 0)
+			{
+				MoveSpeed.Y = MoveDelta;
+				TileRangeX = new Vector2i(2, 3);
+			}
+			if((gamePadData.Buttons & GamePadButtons.Down) != 0)
+			{
+				MoveSpeed.Y = -MoveDelta;
+				TileRangeX = new Vector2i(0, 1);
+			}
+			// Attacks if in battle
+			if(InBattle)
+			{
+				if((gamePadData.ButtonsDown & GamePadButtons.Cross) != 0)
+				{
+					attackState = AttackStatus.MeleeNormal;
+				}
+				if((gamePadData.ButtonsDown & GamePadButtons.Circle) != 0)
+				{
+					attackState = AttackStatus.MeleeStrong;
+				}
+				if((gamePadData.ButtonsDown & GamePadButtons.Square) != 0)
+				{
+					attackState = AttackStatus.RangedNormal;
+				}
+				if((gamePadData.ButtonsDown & GamePadButtons.Triangle) != 0)
+				{
+					attackState = AttackStatus.RangedStrong;
+				}
+				
+				if((gamePadData.ButtonsDown & GamePadButtons.L) != 0)
+				{
+					IsDefending = true;
+				}
+			}
+			// Set frame to start of animation range if outside of range
+			if(TileIndex2D.X < TileRangeX.X || TileIndex2D.X > TileRangeX.Y)
+				TileIndex2D.X = TileRangeX.X;
+		}
+		
+		private void HandleCollision()
+		{
+			if(SceneManager.CurrentScene == null)
+				return;
+			// Loop through tiles
+			foreach(Tile t in Tile.Collisions)
+			{
+				if(t.Overlaps(this))
+				{
+					if(!MoveSpeed.IsZero())
+						t.HandleCollision(Position, ref MoveSpeed);
+					
+					if(t.Key == 'Z')
+						Info.LevelClear = true;
+				}
+			}
+			
+			foreach(EntityAlive e in SceneManager.CurrentScene.Children[1].Children)
+			{
+				if(e.Overlaps(this) && e.IsAlive)
+				{
+					if(!MoveSpeed.IsZero() && e.IsCollidable)
+						MoveSpeed *= -1f;
+					
+					/*if(e.IsBoss && !Info.InBattle)
+					{
+						if(!Info.HadConversation)
+						{
+							SceneManager.ReplaceUIScene(new Conversation());
+						}
+						else
+							e.IsCollidable = false;
+					}
+					else*/
+					if(e.IsAlive)
+					{
+						Opponent = e;
+						Opponent.Opponent = this;
+						Opponent.InBattle = true;
+						//if(!InBattle)
+						//	SceneManager.ReplaceUIScene(new FightUI(this, Opponent));
+						InBattle = true;
+						//Info.InBattle = true;
+					}
+					else
+					{
+						Opponent = null;
+						e.Opponent = null;
+						e.InBattle = false;
+						InBattle = false;
+						//Info.InBattle = false;
+					}
+				}
+			}
+		}
+	}
+}
