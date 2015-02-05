c2bf9a3ee5a903866aa436e290ad943c46e3fce7
Luis Checa <c008101c@student.staffs.ac.uk>
2015-02-05 12:45:49 +0000

    [~] Read Player1 position from level

--- Level.cs
+++ Level.cs
@@ -6,6 +6,7 @@ using Sce.PlayStation.Core.Imaging;
 using Sce.PlayStation.Core.Audio;
 using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
+
 namespace TheATeam
 {
 	public class Level: Scene
@@ -20,74 +21,60 @@ namespace TheATeam
 		private Label lblDebugLeft;
 		private int screenWidth;
 		private int screenHeight;
-		
-		
-		
-		Font font;		
+		Font font;
 		FontMap debugFont;
-		
 
 		public Level(): base()
 		{
-			
+
 			screenWidth = Director.Instance.GL.Context.Screen.Width;
 			screenHeight = Director.Instance.GL.Context.Screen.Height;
-		
+
 			font = new Font(FontAlias.System, 25, FontStyle.Bold);
 			debugFont = new FontMap(font, 25);
-			
+
 			// Reload the font becuase FontMap disposes of it
 			font = new Font(FontAlias.System, 25, FontStyle.Bold);
-			
-			TextureManager.AddAsset("tiles", new TextureInfo(new Texture2D("/Application/assets/tiles.png", false),
-			                                                 new Vector2i(7, 1)));
-//
-			TextureManager.AddAsset("entities", new TextureInfo(new Texture2D("/Application/assets/dungeon_objects.png", false),
-			                                                 new Vector2i(9, 14)));
-//			
-//			// Initial Values;
-			Info.TotalGameTime = 0f;
-			Info.LevelNumber = 1;
 			Info.LevelClear = false;
 			Vector2 cameraCenter = Vector2.Zero;
-			//AddChild(new TextureInfo("/Application/assets/Background.png"));
+
+            AddChild(new Background());
+
 			Tile.Loader("/Application/assets/level3.txt", ref cameraCenter, this);
 			Info.CameraCenter = cameraCenter;
-			
 
-	
-			player1 = new Player(new Vector2(64,300),true);
-			player2 = new Player(new Vector2(960 - 164,300),false);
-			
+			player1 = new Player(cameraCenter, true);
+			player2 = new Player(new Vector2(960 - 164, 300), false);
+
 			if(AppMain.TYPEOFGAME.Equals("MULTIPLAYER"))
 			{
-				
+
 				lblTopLeft = new Label();
 				lblTopLeft.FontMap = debugFont;
 				lblTopLeft.Text = "Player 1";
-				lblTopLeft.Position = new Vector2 (100, screenHeight - 200);
-				
+				lblTopLeft.Position = new Vector2(100, screenHeight - 200);
+
 				lblTopRight = new Label();
 				lblBottomLeft = new Label();
 				lblBottomRight = new Label();
 				lblDebugLeft = new Label();
-				
+
 				lblTopRight.FontMap = debugFont;
 				lblTopRight.Text = "Player 2";
 				lblTopRight.Position = new Vector2(screenWidth - 200, screenHeight - 200);
-				
+
 				lblBottomLeft.FontMap = debugFont;
 				lblBottomLeft.Text = "Waiting";
 				lblBottomLeft.Position = new Vector2(100, 300);
-				
+
 				lblBottomRight.FontMap = debugFont;
 				lblBottomRight.Text = "Waiting";
-				lblBottomRight.Position = new Vector2(screenWidth -200, 300);
-				
+				lblBottomRight.Position = new Vector2(screenWidth - 200, 300);
+
 				lblDebugLeft.FontMap = debugFont;
 				lblDebugLeft.Text = "Waiting for both connections";
 				lblDebugLeft.Position = new Vector2(430, 200);
-				
+
 				this.AddChild(lblTopRight);
 				this.AddChild(lblBottomLeft);
 				this.AddChild(lblBottomRight);
@@ -97,29 +84,29 @@ namespace TheATeam
 			this.AddChild(player1);
 			this.AddChild(player2);
 			Camera2D.SetViewFromViewport();
-			
-			
+
+
 //			Schedule((dt) => {
 //				Info.TotalGameTime += dt;
 //				// Camera2D.SetViewFromHeightAndCenter(Info.CameraHeight, Info.CameraCenter);
 //			});
 		}
-		
-		public override void Update (float dt)
+
+		public override void Update(float dt)
 		{
-			base.Update (dt);
-			
+			base.Update(dt);
+
 			if(AppMain.TYPEOFGAME.Equals("MULTIPLAYER"))
 			{
 				string status = AppMain.client.statusString;
 				if(status.Equals("None"))
 				{
 					AppMain.client.ChangeStatus();
-					lblDebugLeft.Text =  "Changing";	
+					lblDebugLeft.Text = "Changing";
 				}
 				else
-					lblDebugLeft.Text =  status;
-				
+					lblDebugLeft.Text = status;
+
 				if(AppMain.ISHOST)
 				{
 					player1.Update(dt);
@@ -133,28 +120,28 @@ namespace TheATeam
 					player1.Update(dt);
 				}
 			}
-			else if (AppMain.TYPEOFGAME.Equals("SINGLE"))
+			else if(AppMain.TYPEOFGAME.Equals("SINGLE"))
 			{
-					player1.Update(dt);
-					player2.UpdateAI(dt,player1);
+				player1.Update(dt);
+				player2.UpdateAI(dt, player1);
 			}
-			
-			
+
+
 			// handle bullet update and collision
 			ProjectileManager.Instance.Update(dt);
-			
+
 			if(ProjectileManager.Instance.ProjectileCollision(player1.Position, player1.Quad.Bounds2()))
 				Console.WriteLine("Player 1 got hit");
 			if(ProjectileManager.Instance.ProjectileCollision(player2.Position, player2.Quad.Bounds2()))
 				Console.WriteLine("Player 2 got hit");
-			
-			
+
+
 			foreach(Tile t in Tile.Collisions)
 			{
-				if(ProjectileManager.Instance.ProjectileCollision(t.Position, t.Quad.Bounds2())) 
+				if(ProjectileManager.Instance.ProjectileCollision(t.Position, t.Quad.Bounds2()))
 					Console.WriteLine("bullet hit tile"); // add tile.damage(); here **can hit more then 1 tile at a time**
 			}
-			
+
 			ItemManager.Instance.Update(dt);
 			ItemManager.Instance.ItemCollision(player1.Position, player1.Quad.Bounds2());
 
