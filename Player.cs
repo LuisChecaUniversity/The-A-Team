6a4b6b8c56fa9502cab19089fe591c1e0f7e1f2d
pscorgie <s031789c@student.staffs.ac.uk>
2015-04-23 10:53:15 +0100

    [+] Keyboard test off [PS]

--- Player.cs
+++ Player.cs
@@ -32,7 +32,7 @@ namespace TheATeam
 		private static float MoveDelta = 4f;
 		new static Vector2 boundsScale = new Vector2(1f);
 		protected bool canShoot = true;
-		private bool keyboardTest = true;
+		private bool keyboardTest = false;
 		private char _element, _element2;
 		protected Vector2 Direction;
 		protected Vector2 ShootingDirection;
